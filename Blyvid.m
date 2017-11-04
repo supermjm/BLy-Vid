a3072b8d0777e4718ce8095e62699dce988720f0
Mike Martin <supermjm9@gmail.com>
2017-11-04 11:00:06 -0700

    Replacing lines. First working version
    Working version of replacing lines from one field with the line below from the other

--- Blyvid.m
+++ Blyvid.m
@@ -1,3 +1,37 @@
-function Blyvid
+function Blyvid(vid_in, vid_out, rlines)
+% Blyvid will replace the video lines from video file vid_in, specified by
+% number in rlines with a duplicate of the line below it. It writes the
+% output to the file named in vid_out
 
+% setup some defaults
+    switch nargin
+        case 0
+            vid_in = 'Btest.mp4'; vid_out = 'Ofile.mp4';
+            rlines = 220:2:250;
+        case 1
+            vid_out = 'Ofile.mp4';
+            rlines = 220:2:250;
+        case 2
+            rlines = 220:2:250;
+    end
+    
+    % setup the input file
+    vidInObj = VideoReader(vid_in);
+    % and the output file
+    vidOutObj = VideoWriter(vid_out,'MPEG-4');
+    vidOutObj.FrameRate = vidInObj.FrameRate;
+    open(vidOutObj);
+    
+    while hasFrame(vidInObj)
+        % get the current frame
+        fr = readFrame(vidInObj);
+        % for each line in rlines, replace it with the line below
+        fr(rlines,:,:) = fr(rlines+1,:,:);
+        % write the modified video frame
+        writeVideo(vidOutObj,fr);
+    end
+    
+    % close the output file
+    close(vidOutObj);
+    
 end
